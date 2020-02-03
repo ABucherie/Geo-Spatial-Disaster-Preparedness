b34dace641b6e61dbe3f81ec604f184c7e532a22
Agathe Bucherie <57904822+ABucherie@users.noreply.github.com>
2020-02-03 13:51:46 +0100

    Add files via upload

--- IBF_flood_model_performance_visual.py
+++ IBF_flood_model_performance_visual.py
@@ -6,32 +6,39 @@ Created on Sat Jan 18 11:12:23 2020
 """
 #%%
 # setting up your environment
-import xarray as xr
+
 import pandas as pd
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
-import numpy as np
-import os
-from os import listdir
-from os.path import isfile, join
 import geopandas as gpd
-from shapely.geometry import Point
-from osgeo import ogr
-import xshape
-import rasterio
-from rasterstats import zonal_stats
-import fiona
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
-#%% 
-# sources
-model_performance = 'C:/CODE_510/statistical_floodimpact_uganda-Ghitub/output/uganda/Performance_scores/uga_glofas_performance_score.csv'
-district_uganda = 'C:/CODE_510/statistical_floodimpact_uganda-Ghitub/shapes/uga_admbnda_adm1/uga_admbnda_adm1_UBOS_v2.shp'
+#%% Cell to change per country
+    
+country = 'Kenya'  
+ct_code='ken'
+
+#Path name to the folder : 
+path = my_local_path + '/' + country + '/'
+#path= 'C:/CODE_510/V111_glofas/%s/' %country
+
+# Read the path to the relevant admin level shape to use for the study
+
+#for Uganda activate the following lines :
+#Admin= path + 'input/Admin/uga_admbnda_adm1_UBOS_v2.shp'   # for Uganda
+#Admin_col = 'ADM1_EN'  # column name of the Admin name in the shapefile of Uganda
+
+#for Kenya activate the following lines :
+Admin= path + 'input/Admin/KEN_adm1_mapshaper_corrected.shp' # for Kenya
+Admin_col = 'name'  # column name of the Admin name in the shapefile for Kenya
+
+# sources of the model perforfance results from the previous script V111_glofas
+model_performance = path + 'output/Performance_scores/%s_glofas_performance_score.csv' %ct_code
 
 #%% 
 # Open the district admin 1 sapefile
-district= gpd.read_file(district_uganda)
+district= gpd.read_file(Admin)
 #open the csv file with performance results of the model
 model_perf =pd.read_csv(model_performance)
 
@@ -40,11 +47,11 @@ best= model_perf.groupby(['district', 'quantile'])['far'].transform(min)==model_
 model_perf_best= model_perf[best]
 
 # lower case the district column in both file and merge
-district['ADM1_EN' ]= district['ADM1_EN'].str.lower()
+district[Admin_col]= district[Admin_col].str.lower()
 model_perf_best['district']= model_perf_best['district'].str.lower()
 
-merged_perf= district.set_index('ADM1_EN').join(model_perf_best.set_index('district')) 
-merged_perf.to_file('C:/CODE_510/statistical_floodimpact_uganda-Ghitub/output/uganda/Performance_scores/perf_uga_v111.shp')
+merged_perf= district.set_index(Admin_col).join(model_perf_best.set_index('district')) 
+merged_perf.to_file(path + 'output/Performance_scores/perf_%s_v111.shp' %ct_code)
 
 #%%  create a figure with the number of flood event recorded per district
 fig, ax=plt.subplots(1,figsize=(10,10))
@@ -53,27 +60,22 @@ cax = divider.append_axes("right", size="5%", pad=0.2)
 
 merged_perf.plot(ax=ax, color='lightgrey', edgecolor='grey')
 ax.set_title('Number of recorded flood event per district', fontsize= 14)
-cmap = cm.get_cmap('jet', 10)
-#cmap = cm.get_cmap('gist_ncar_r', 10)
+cmap = cm.get_cmap('jet', 60)    # adapt the number if needed
 
 perfdrop= merged_perf.dropna(subset=['nb_event'])
-perfdrop.plot(ax=ax,column='nb_event', legend= True,vmin=1,vmax=10, cmap=cmap, cax=cax)
+perfdrop.plot(ax=ax,column='nb_event', legend= True,vmin=1,vmax=60, cmap=cmap, cax=cax)    # adapt the vmax number if needed
 
-fig.savefig('C:/CODE_510/statistical_floodimpact_uganda-Ghitub/output/uganda/Performance_scores/Nb_event_district.png')
+fig.savefig(path+'output/Performance_scores/Nb_event_district.png')
 
-#%% 
-#choose the quantile to plot 
-#perf_quantile= merged_perf[merged_perf['quantile']== 'Q90_pred']
-
-quantiles = ['Q90_pred', 'Q95_pred', 'Q98_pred']
+#%% create 4 maps representing the results of the glofas model performance per district (POD, FAR, POFD and CSI)
+quantiles = ['Q50_pred', 'Q80_pred', 'Q90_pred']
 
 for quantile in quantiles:
     
     perf_quantile= merged_perf[merged_perf['quantile']== quantile]
     
     fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, figsize=(16,16))
-    #title("Title", line = -2)
-    fig.suptitle('Performance results of model v1.1.1 in Uganda (%s Glofas)'%quantile,fontsize= 22,fontweight= 'bold', x=0.5, y=0.94)   
+    fig.suptitle('Performance results of model v1.1.1 in %s (%s Glofas)' %(country, quantile),fontsize= 22,fontweight= 'bold', x=0.5, y=0.94)   
     divider_ax1 = make_axes_locatable(ax1)
     divider_ax2 = make_axes_locatable(ax2)
     divider_ax3 = make_axes_locatable(ax3)
@@ -100,15 +102,9 @@ for quantile in quantiles:
     perf_quantile.plot(ax=ax4,column='pod', legend= True, cmap='coolwarm_r', cax=cax4)
     
 
-    fig.savefig('C:/CODE_510/statistical_floodimpact_uganda-Ghitub/output/uganda/Performance_scores/Uganda_v111_%s.png' %quantile)
+    fig.savefig(path + 'output/Performance_scores/%s_v111_%s.png' % (country, quantile))
 
 
-#%% try multiplot 
-merged_perf.plot(ax=ax, color='lightgrey', edgecolor='grey')
-
-ax.set_title('FAR results of model v1.1.1 in Uganda (Glofas only)', fontsize= 12)
-perfdrop= merged_perf90.dropna(subset=['far'])
-perfdrop.plot(ax=ax,column='far', legend= True, cmap='coolwarm', cax=cax)
 
 
 
